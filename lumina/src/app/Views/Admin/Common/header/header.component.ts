9c289fd1d007589620118d78df64a800f15e6acf
TuyenTV0412 <Trungtuyen2003vp@gmail.com>
2025-10-14 21:03:49 +0700

    manage question

--- lumina/src/app/Views/Admin/Common/header/header.component.ts
+++ lumina/src/app/Views/Admin/Common/header/header.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../../../Services/Auth/auth.service';
 import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
@@ -12,8 +13,9 @@ import { filter, map, mergeMap } from 'rxjs/operators';
 export class HeaderComponent implements OnInit {
   @Output() sidebarToggle = new EventEmitter<void>();
   pageTitle = 'Admin Panel';
+    isDropdownOpen = false;
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private authService: AuthService) {}
 
   ngOnInit() {
     this.router.events.pipe(
@@ -27,5 +29,14 @@ export class HeaderComponent implements OnInit {
     ).subscribe(data => {
       this.pageTitle = data['title'] || 'Admin Panel';
     });
+  }
+  toggleDropdown(event: Event): void {
+    event.stopPropagation();
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+    logout(): void {
+    this.isDropdownOpen = false;
+    this.authService.logout();
   }
 }
